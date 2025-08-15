c48c5c3fd6735b212d1f0d79a191c4c6a2affd10
Sumon Ahmed <sumonahmed012340@gmail.com>
2025-08-16 00:00:00 +0600

    fake data for role

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,4 @@
+import { role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -119,21 +120,25 @@ const menuItems = [
 const Menu = () => {
   return (
     <div className="mt-4 text-sm pl-2">
-      {menuItems.map((item) => (
-        <div className=" flex flex-col gap-2" key={item.title}>
-          <span className="hidden md:block lg:block text-gray-400 my-4 font-light ">
-            {item.title}
+      {menuItems.map((i) => (
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {i.title}
           </span>
-          {item.items.map((item) => (
-            <Link
-              href={item.href}
-              key={item.label}
-              className="flex gap-4 items-center justify-center lg:justify-start text-gray-500 py-2"
-            >
-              <Image src={item.icon} alt="img" width={20} height={20} />
-              <span className="hidden md:block lg:block">{item.label}</span>
-            </Link>
-          ))}
+          {i.items.map((item) => {
+            if (item.visible.includes(role)) {
+              return (
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md odd:hover:bg-[#EDF9FD] even:hover:bg-[#F1F0FF]   "
+                >
+                  <Image src={item.icon} alt="" width={20} height={20} />
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              );
+            }
+          })}
         </div>
       ))}
     </div>
