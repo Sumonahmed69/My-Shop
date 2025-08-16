3183037679e28db5e34cbab20abf214bd38cb5d1
Sumon Ahmed <sumonahmed012340@gmail.com>
2025-08-16 21:22:07 +0600

    Parent Dashboard Design

--- src/app/(dashborad)/parent/page.tsx
+++ src/app/(dashborad)/parent/page.tsx
@@ -1,9 +1,23 @@
+import Announcement from "@/components/Announcement";
+import BigCalendar from "@/components/BigCalendar";
+
 
 const ParentPage = () => {
     return (
-        <div className=" text-7xl mt-16 text-center">
-           ParentPage
+ <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* left */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+<h1 className="text-xl font-semibold">Schedule (John Doe)</h1>
+          <BigCalendar></BigCalendar>
         </div>
+      </div>
+      {/* right */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8 ">
+
+        <Announcement></Announcement>
+      </div>
+    </div>
     );
 };
 
