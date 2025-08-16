851b794820aa4142b01f47f88d56e215f51a9bf2
Sumon Ahmed <sumonahmed012340@gmail.com>
2025-08-16 22:39:53 +0600

    List Table Design with Pagination

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   images: {
+    domains: [
+      "images.pexels.com", // 👈 add this
+    ],
+  },
 };
 
 export default nextConfig;
